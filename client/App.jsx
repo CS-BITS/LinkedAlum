51c581338122783fbe89dcf24bf72a9ee324fce1
Tommy Ngo <tommyhoaingo@gmail.com>
2022-03-01 14:42:06 -0600

    init route config

--- client/App.jsx
+++ client/App.jsx
@@ -1,11 +1,18 @@
 import React from 'react';
+import Mainpage from './components/MainPage';
 import Feed from './pages/Feed';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Link } from 'react-router-dom';
+
 export default function App() {
   return (
-    <div className='AppContainer'>
-      {/* gotta route somehow */}
-      <div>Landing?</div>
-      <Feed />
-    </div>
+    <Router>
+      <Routes>
+        <Route path='/' element={<Mainpage />} />
+        <Route path='/feed' element={<Feed />} />
+        {/* <Route path='/'><Mainpage/> </Route> */}
+        {/* <Route path='/feed' component={Feed}/> */}
+      </Routes>
+    </Router>
   );
 }
---
+++ client/components/MainPage.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import GoogleSign from './images/GoogleSign.png'
+import GitHubLogin from './images/github.png'
+import Navbar from './NavBar'
+const Mainpage = () => {
+    function GoogleButton(){
+        console.log('Google Button is Clicked')
+    }
+    function GithubButton(){
+        console.log('GitHub Button is Clicked')
+    }
+return (
+    <div>
+        <Navbar/>
+    <div className='bg-white h-screen flex flex-col justify-center items-center'>
+        <h2 className='lg:text-9xl md:text-7xl sm:text-5xl text-3xl font-black mb-14'>
+            LinkedAlum
+        </h2>
+        <h3 className='font-black'>
+            Connecting People
+        </h3>
+        <br></br>
+        <button>
+            <img className='h-12 w-38'src={GoogleSign} onClick={GoogleButton} />
+        </button>
+        <button>
+            <img className='h-12 w-38'src={GitHubLogin} onClick={GithubButton} />
+        </button>
+    </div>
+    </div>
+  );
+};
+export default Mainpage;
