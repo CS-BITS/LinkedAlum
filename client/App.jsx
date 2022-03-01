83e51e2dd801215293f569557137e540647111d4
Tommy Ngo <tommyhoaingo@gmail.com>
2022-03-01 15:02:40 -0600

    barebones feed layout

--- client/App.jsx
+++ client/App.jsx
@@ -1,18 +1,12 @@
 import React from 'react';
-import Mainpage from './components/MainPage';
 import Feed from './pages/Feed';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { Link } from 'react-router-dom';
 
 export default function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path='/' element={<Mainpage />} />
-        <Route path='/feed' element={<Feed />} />
-        {/* <Route path='/'><Mainpage/> </Route> */}
-        {/* <Route path='/feed' component={Feed}/> */}
-      </Routes>
-    </Router>
+    <>
+      <Feed />
+    </>
   );
 }
