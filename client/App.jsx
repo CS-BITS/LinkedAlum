856210179ca05e26f57283ccad29133aff806e43
Tommy Ngo <tommyhoaingo@gmail.com>
2022-03-01 14:11:40 -0600

    save progress
    Co-authored-by: Brianzheng123 <Brianzheng123@users.noreply.github.com>

--- client/App.jsx
+++ client/App.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import Counter from './components/Counter';
 import Feed from './pages/Feed';
 export default function App() {
   return (
--- client/index.js
+++ client/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import App from './App.jsx';
 // import './index.css'
 import { Provider } from 'react-redux';
 import store from './store';
